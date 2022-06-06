f5b5fffdbaab36611d883077b28c42fda8b6c9c7
mahdi <mahdihadjsassi@fsb.u-carthage.tn>
2022-06-06 11:17:00 +0100

    hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-    production: true
+    production: true,
+
+    apiBaseUrl: 'http://localhost:8081',
+    apiBaseUrlUpload: 'http://localhost:8081/upload/',
+    admine: 'mahdihadjsassi1@gmail.com',
+    firebase: {
+        apiKey: 'AIzaSyCUa1-RGYctGMX9T44QHhTPVQL43whB60I',
+        authDomain: 'u-book-6ac29.firebaseapp.com',
+        projectId: 'u-book-6ac29',
+        storageBucket: 'u-book-6ac29.appspot.com',
+        messagingSenderId: '285478163789',
+        appId: '1:285478163789:web:e6f2cc43bde72648f7d304',
+        measurementId: 'G-9R4LGNCSSP'
+    }
 };
